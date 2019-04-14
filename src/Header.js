34cd4ee24ae2051d0338e9f3513705eb31c85d2d
Gytis Daujotas <gytdau@gmail.com>
2019-04-14 12:44:40 +0100

    Updated header

--- src/Header.js
+++ src/Header.js
@@ -6,10 +6,24 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class Header extends Component {
   render() {
     return (
-      <div style={{backgroundColor: '#56CCDD'}}>
-        <img src='irent_logo.png'/>
-        <p><strong> Irent </strong></p>
-      </div>
+
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div className="container">
+            <ul class="navbar-nav">
+              <li className="nav-item active icon-container">
+                <img src='irent_logo.png' class="navbar-icon" />
+              </li>
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Irent</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
